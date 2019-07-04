ecbce18b4c50cedbb1361e865e66055b95c0adb2
ztw <852445892@qq.com>
2019-07-04 22:06:23 +0800

    修改命令行参数输入格式

--- src/main/java/cn/nju/st13/preprocess/Preprocess.java
+++ src/main/java/cn/nju/st13/preprocess/Preprocess.java
@@ -8,19 +8,19 @@ public class Preprocess {
             System.err.println("error!");
             System.exit(1);
         }
-        WordSeg.Task1Main(new Path(args[0]), new Path(args[1]), new Path(args[2] + "output1"));
+        WordSeg.Task1Main(new Path(args[0]), new Path(args[1]), new Path(args[2] + "/output1"));
         switch (args[3]) {
             case "-1":
-                WordCoOcc_baseline.Task2Main(new Path(args[2] + "output1"), new Path(args[2] + "output2"));
-                Normalization_inv.Task3Main(new Path(args[2] + "output2"), new Path(args[2] + "output3"));
+                WordCoOcc_baseline.Task2Main(new Path(args[2] + "/output1"), new Path(args[2] + "/output2"));
+                Normalization_inv.Task3Main(new Path(args[2] + "/output2"), new Path(args[2] + "/output3"));
                 break;
             case "-2":
-                WordCoOcc_pair.Task2Main(new Path(args[2] + "output1"), new Path(args[2] + "output2"));
-                Normalization.Task3Main(new Path(args[2] + "output2"), new Path(args[2] + "output3"));
+                WordCoOcc_pair.Task2Main(new Path(args[2] + "/output1"), new Path(args[2] + "/output2"));
+                Normalization.Task3Main(new Path(args[2] + "/output2"), new Path(args[2] + "/output3"));
                 break;
             case "-3":
-                WordCoOcc.Task2Main(new Path(args[2] + "output1"), new Path(args[2] + "output2"));
-                Normalization_inv.Task3Main(new Path(args[2] + "output2"), new Path(args[2] + "output3"));
+                WordCoOcc.Task2Main(new Path(args[2] + "/output1"), new Path(args[2] + "/output2"));
+                Normalization_inv.Task3Main(new Path(args[2] + "/output2"), new Path(args[2] + "/output3"));
                 break;
         }
     }
