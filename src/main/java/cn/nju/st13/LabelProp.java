1348236fed5bb65b72c58f73eb6330b1e2d98e18
BYGX-wcr <wcr@live.cn>
2019-07-05 20:50:56 +0800

    finish task 5

--- src/main/java/cn/nju/st13/LabelProp.java
+++ src/main/java/cn/nju/st13/LabelProp.java
@@ -15,11 +15,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-class LabelProp {
+public class LabelProp {
     static class InitMapper extends Mapper <Object, Text, Text, Text> {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] content = value.toString().split(" |\t", 1);
+            String[] content = value.toString().split(" |\\t");
             String node = content[0];
             String label = String.valueOf(node.hashCode());
 
@@ -41,10 +41,10 @@ class LabelProp {
     static class IterMapper extends Mapper <Object, Text, Text, Text> {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] content = value.toString().split(" |\t", 2);
+            String[] content = value.toString().split(" |\\t");
             String node = content[0];
             String label = content[1];
-            String[] edgeList = content[2].split(" ");
+            String[] edgeList = content[2].split("\\|");
 
             //emit node's label to its neighbors
             for (String edge : edgeList) {
@@ -69,39 +69,42 @@ class LabelProp {
 
             //resolve data, build labelMap & get edgeList
 			for (Text val : values) {
-                String[] content = val.toString().split(" ", 1);
+                String[] content = val.toString().split(" ");
 				if (content[0].equals("label")) {
                     String neighbor = content[1].split(",")[0];
                     Integer neighborLabel = new Integer(content[1].split(",")[1]);
                     labelMap.put(neighbor, neighborLabel);
                 }
                 else if (content[0].equals("edgeList")) {
-                    edgeList = content[1].split(" ");
+                    edgeList = content[1].split("\\|");
                     edgeListStr = content[1];
                 }
                 else {
-                    System.err.println("Undefined prefix: " + content[0]);
+                    throw new IOException("Undefined prefix: " + content[0]);
                 }
             }
             
-            assert(edgeList != null);
+            if (edgeList.length == 0) throw new IOException("edgeList is empty");
 
             //build weightMap
             int edgeNum = edgeList.length;
             int counter = 0;
             int brokenTie = randGen.nextInt(edgeNum);
+            if (edgeNum == 1) brokenTie = -1;
             for (String edge : edgeList) {
-                if (counter == brokenTie) continue;
+                //randomly break tie
+                if (counter != brokenTie) {
+                    String neighbor = edge.split(",")[0];
+                    Double weight = new Double(edge.split(",")[1]);
+                    Integer neigborLabel = labelMap.get(neighbor);
+                    if (neigborLabel == null) throw new IOException("neighborLabel is null");
 
-                String neighbor = edge.split(",")[0];
-                Double weight = new Double(edge.split(",")[1]);
-                Integer neigborLabel = labelMap.get(neighbor);
-
-                if (weightMap.containsKey(neigborLabel)) { // add
-                    weightMap.put(neigborLabel, weightMap.get(neigborLabel) + weight);
-                }
-                else { // init
-                    weightMap.put(neigborLabel, weight);
+                    if (weightMap.containsKey(neigborLabel)) { // add
+                        weightMap.put(neigborLabel, weightMap.get(neigborLabel) + weight);
+                    }
+                    else { // init
+                        weightMap.put(neigborLabel, weight);
+                    }
                 }
 
                 counter++;
@@ -109,13 +112,15 @@ class LabelProp {
 
             //find maximum weight label
             Integer maxLabel = new Integer(-1);
-            Double maxValue = new Double(-1);
+            Double maxValue = new Double(-1.0);
             for (HashMap.Entry<Integer, Double> entry : weightMap.entrySet()) {
                 if (entry.getValue() > maxValue) {
                     maxValue = entry.getValue();
                     maxLabel = entry.getKey();
                 }
             }
+
+            if (maxLabel == -1) throw new IOException("maxLabel cannot be found with map size " + weightMap.size() + " counter " + counter + " length " + edgeList.length);
 
             context.write(key, new Text(maxLabel.toString() + " " + edgeListStr));
 		}
@@ -124,7 +129,7 @@ class LabelProp {
     static class ClusterMapper extends Mapper <Object, Text, Text, Text> {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] content = value.toString().split(" |\t", 2);
+            String[] content = value.toString().split(" |\\t", 3);
             String node = content[0];
             String label = content[1];
 
@@ -141,7 +146,7 @@ class LabelProp {
 		}
     }
 
-    static void initialize(String inputPath, String outputPath) {
+    static void initialize(Path inputPath, Path outputPath) {
         Configuration configuration = new Configuration();
 
         Job initJob = null;
@@ -156,15 +161,15 @@ class LabelProp {
         initJob.setReducerClass(InitReducer.class);
         initJob.setInputFormatClass(TextInputFormat.class);
         initJob.setNumReduceTasks(5);
-        // initJob.setOutputKeyClass(Text.class);
-        // initJob.setOutputValueClass(IntWritable.class);
+        initJob.setOutputKeyClass(Text.class);
+        initJob.setOutputValueClass(Text.class);
         try {
-            FileInputFormat.addInputPath(initJob, new Path(inputPath));
+            FileInputFormat.addInputPath(initJob, inputPath);
         }
         catch(IOException ioe) {
             ioe.printStackTrace();
         }
-        FileOutputFormat.setOutputPath(initJob, new Path(outputPath));
+        FileOutputFormat.setOutputPath(initJob, outputPath);
 
         try {
             initJob.waitForCompletion(true);
@@ -174,12 +179,12 @@ class LabelProp {
         }
     }
 
-    static void iterate(String inputPath, String outputPath) {
+    static void iterate(Path inputPath, Path outputPath, int epochId) {
         Configuration configuration = new Configuration();
 
         Job iterJob = null;
         try {
-            iterJob = Job.getInstance(configuration, "2019St13 LabelProp Iteration Job");
+            iterJob = Job.getInstance(configuration, "2019St13 LabelProp Iteration Job epoch " + String.valueOf(epochId));
         }
         catch(IOException ioe) {
             ioe.printStackTrace();
@@ -189,15 +194,15 @@ class LabelProp {
         iterJob.setReducerClass(IterReducer.class);
         iterJob.setInputFormatClass(TextInputFormat.class);
         iterJob.setNumReduceTasks(5);
-        // iterJob.setOutputKeyClass(Text.class);
-        // iterJob.setOutputValueClass(IntWritable.class);
+        iterJob.setOutputKeyClass(Text.class);
+        iterJob.setOutputValueClass(Text.class);
         try {
-            FileInputFormat.addInputPath(iterJob, new Path(inputPath));
+            FileInputFormat.addInputPath(iterJob, inputPath);
         }
         catch(IOException ioe) {
             ioe.printStackTrace();
         }
-        FileOutputFormat.setOutputPath(iterJob, new Path(outputPath));
+        FileOutputFormat.setOutputPath(iterJob, outputPath);
 
         try {
             iterJob.waitForCompletion(true);
@@ -207,7 +212,7 @@ class LabelProp {
         }
     }
 
-    static void cluster(String inputPath, String outputPath) {
+    static void cluster(Path inputPath, Path outputPath) {
         Configuration configuration = new Configuration();
 
         Job clusterJob = null;
@@ -218,18 +223,18 @@ class LabelProp {
             ioe.printStackTrace();
         }
         clusterJob.setJarByClass(LabelProp.class);
-        clusterJob.setMapperClass(InitMapper.class);
-        clusterJob.setReducerClass(InitReducer.class);
+        clusterJob.setMapperClass(ClusterMapper.class);
+        clusterJob.setReducerClass(ClusterReducer.class);
         clusterJob.setInputFormatClass(TextInputFormat.class);
-        // clusterJob.setOutputKeyClass(Text.class);
-        // clusterJob.setOutputValueClass(IntWritable.class);
+        clusterJob.setOutputKeyClass(Text.class);
+        clusterJob.setOutputValueClass(Text.class);
         try {
-            FileInputFormat.addInputPath(clusterJob, new Path(inputPath));
+            FileInputFormat.addInputPath(clusterJob, inputPath);
         }
         catch(IOException ioe) {
             ioe.printStackTrace();
         }
-        FileOutputFormat.setOutputPath(clusterJob, new Path(outputPath));
+        FileOutputFormat.setOutputPath(clusterJob, outputPath);
 
         try {
             clusterJob.waitForCompletion(true);
@@ -240,22 +245,36 @@ class LabelProp {
     }
     
     public static void main(String[] args) {
-        String inputPath = args[0];
-        String outputPath = args[1];
-        int epoch = 100;
-
-        initialize(inputPath, outputPath + ".tempout");
-
-        for (int i = 0; i < epoch; ++i) {
-            iterate(outputPath + ".tempout", outputPath + ".tempout");
-        }
-
-        cluster(outputPath + ".tempout", outputPath);
-
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+        int epoch = 2;
         try {
-            FileSystem fs = new Path(args[1] + ".tempout").getFileSystem(new Configuration());
-            if(fs.exists(new Path(args[1] + ".tempout"))){
-                    fs.delete(new Path(args[1] + ".tempout"), true); 
+            Configuration configuration = new Configuration();
+            Path tempin = new Path(outputPath + ".tempin");
+            Path tempout = new Path(outputPath + ".tempout");
+
+            initialize(inputPath, tempout);
+
+            for (int i = 0; i < epoch; ++i) {
+                FileSystem fs = tempout.getFileSystem(configuration);
+                if (fs.exists(tempout)) {
+                    fs.rename(tempout, tempin);
+                }
+                iterate(tempin, tempout, i + 1);
+                if (fs.exists(tempin)) {
+                    fs.delete(tempin, true); 
+                }
+            }
+
+            cluster(tempout, outputPath);
+
+            //delete immediate files
+            FileSystem fs = tempout.getFileSystem(configuration);
+            if (fs.exists(tempin)) {
+                fs.delete(tempin, true); 
+            }
+            if (fs.exists(tempout)) {
+                fs.delete(tempout, true); 
             }
         }
         catch (Exception e) {
